cec999e5159035b7f866c4b1a491c6e65af11309
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-03-30 21:34:17 -0300

    adjustments

--- class353-watched/scripts.js
+++ class353-watched/scripts.js
@@ -1,11 +1,28 @@
 new Vue({
   el: "#app",
   data: {
-    link: "http://google.com",
+    name: "Nome",
+    counter: 0,
+    secondCounter: 0,
+  },
+  computed: {
+    output: function () {
+      console.log("Computed")
+      return this.counter > 5 ? "Greater than 5" : "Smaller than 5"
+    },
+  },
+  watch: {
+    counter: function (valeu) {
+      let vm = this
+      setTimeout(() => {
+        vm.counter = 0
+      }, 2000)
+    },
   },
   methods: {
-    changeLink: function () {
-      this.link = "http://apple.com"
+    result: function () {
+      console.log("Method")
+      return this.counter > 5 ? "Greater than 5" : "Smaller than 5"
     },
   },
 })
