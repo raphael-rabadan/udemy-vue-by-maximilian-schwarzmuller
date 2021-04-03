7a646a2c5a56b77672a5bdd4d75b2561db31ea8b
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-02 22:21:46 -0300

    until class 424

---
+++ section29-components/vue-cli/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <app-server-status></app-server-status>
+    <app-server-status></app-server-status>
+    <app-server-status></app-server-status>
+    <app-server-status></app-server-status>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
