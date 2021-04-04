a4080ea082114edaed5da52e9ae95822c2b1acee
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-03 23:09:58 -0300

    before doing exercise of section 31

---
+++ section31-advanced-component-usage/example/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr />
+        <p>{{ selectedComponent }}</p>
+        <!-- <app-quote v-for="n in 1" :key="n">
+          <h2 slot="title">The Quote {{ n }}</h2>
+          <p>A wonderful Quote</p>
+        </app-quote> -->
+        <keep-alive>
+          <component :is="selectedComponent"><p>Default content</p></component>
+        </keep-alive>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue"
+import Author from "./components/Author.vue"
+import New from "./components/New.vue"
+
+export default {
+  data() {
+    return {
+      selectedComponent: "appQuote",
+    }
+  },
+  components: {
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New,
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ section31-advanced-component-usage/example/src/components/New.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>New</h3>
+    <button @click="counter++">Increase!</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  destroyed() {
+    console.log("Destroyed")
+  },
+  deactivated() {
+    console.log("deactivated")
+  },
+  activated() {
+    console.log("activated")
+  },
+}
+</script>
+<style></style>
