485e5d1d0daa5b32dfa727ec6d13c8e740c7663a
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-04 17:07:49 -0300

    class 493

--- section34-directives/example/src/main.js
+++ section34-directives/example/src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue"
 import App from "./App.vue"
 
-Vue.directive("highlight", {}) //v-highlight
+Vue.directive("highlight", {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = "green"
+  },
+}) //v-highlight
 new Vue({
   el: "#app",
   render: (h) => h(App),
