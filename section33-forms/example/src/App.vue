a79dc3e41b06dc0cd3b0161a031a971fc78cb712
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-04 14:50:55 -0300

    class 485

--- section33-forms/example/src/App.vue
+++ section33-forms/example/src/App.vue
@@ -102,6 +102,13 @@
           </select>
         </div>
       </div>
+      <div class="row">
+        <div
+          class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
+        >
+          <app-switch v-model="dataSwitch"></app-switch>
+        </div>
+      </div>
       <hr />
       <div class="row">
         <div
@@ -131,7 +138,7 @@
             </ul>
             <p>Gender: {{ gender }}</p>
             <p>Priority: {{ selectedPriority }}</p>
-            <p>Switched:</p>
+            <p>Switched: {{ dataSwitch }}</p>
           </div>
         </div>
       </div>
@@ -140,6 +147,8 @@
 </template>
 
 <script>
+import Switch from "./Switch.vue"
+
 export default {
   data() {
     return {
@@ -153,7 +162,11 @@ export default {
       gender: "Male",
       selectedPriority: "High",
       priorities: ["High", "Medium", "Low"],
+      dataSwitch: true,
     }
+  },
+  components: {
+    appSwitch: Switch,
   },
   methods: {
     submit() {
