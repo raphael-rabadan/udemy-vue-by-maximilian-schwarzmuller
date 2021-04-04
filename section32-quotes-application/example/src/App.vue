07156d5b2c3b56782202fdaa57b64e0b4400fa0f
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-04 11:45:08 -0300

    exercise completed

--- section32-quotes-application/example/src/App.vue
+++ section32-quotes-application/example/src/App.vue
@@ -1,14 +1,51 @@
 <template>
-    <div class="container">
-    
+  <div class="container">
+    <app-header :maxQuotes="maxQuotes" :quoteCount="quotes.length"></app-header>
+    <hr />
+    <app-new-quote @newQuoteWasCreated="addTheNewQuote"></app-new-quote>
+    <hr />
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteWasDeleted="deleteQuote"
+    ></app-quote-grid>
+    <hr />
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
     </div>
+  </div>
 </template>
-
 <script>
-    export default {
-        
+import QuoteGrid from "./components/QuoteGrid.vue"
+import NewQuote from "./components/NewQuote.vue"
+import Header from "./components/Header.vue"
+
+export default {
+  data() {
+    return {
+      quotes: ["Just a quote to see something"],
+      maxQuotes: 10,
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header,
+  },
+  methods: {
+    addTheNewQuote(quote) {
+      if (this.quotes.length < this.maxQuotes) {
+        this.quotes.unshift(quote)
+      } else {
+        window.alert(`Limit of ${this.maxQuotes} quotes was exceeded`)
+      }
+    },
+    deleteQuote(i) {
+      this.quotes.splice(i, 1)
+    },
+  },
+}
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ section32-quotes-application/example/src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <appQuote
+      v-for="(quote, i) in quotes"
+      :key="quote + i"
+      @click.native="deleteQuote(i)"
+    >
+      {{ quote }}
+    </appQuote>
+  </div>
+</template>
+<script>
+import Quote from "./Quote.vue"
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote,
+  },
+  methods: {
+    deleteQuote(i) {
+      this.$emit("quoteWasDeleted", i)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
