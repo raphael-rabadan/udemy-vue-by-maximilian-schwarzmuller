395533b03305f6b15896106e51d9a4ee2b0226e9
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-04 17:02:14 -0300

    class 491

--- section34-directives/example/src/App.vue
+++ section34-directives/example/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'Some <strong>strong</strong> Text'"></p>
+      </div>
     </div>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p>Color this</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
