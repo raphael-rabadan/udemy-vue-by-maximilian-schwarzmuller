fb0493b3f609ece3ee25916d04eebceb037c9775
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-05 17:46:17 -0300

    exercise complete

--- section34-directives/exercise/src/App.vue
+++ section34-directives/exercise/src/App.vue
@@ -1,19 +1,25 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 v-myon:click="clickFunction" style="cursor: pointer">
+          Directives Exercise
+        </h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {}
+export default {
+  methods: {
+    clickFunction() {
+      console.log("Hello, I'm a function")
+    },
+  },
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- section34-directives/exercise/src/main.js
+++ section34-directives/exercise/src/main.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue"
+import App from "./App.vue"
+
+Vue.directive("myon", {
+  bind(el, binding, vnode) {
+    if (binding.arg === "click") {
+      if (typeof binding.value === "function") {
+        el.addEventListener(binding.arg, binding.value)
+      }
+    }
+  },
+  unbind(el, binding, vnode) {
+    el.removeEventListener(binding.arg, binding.value)
+  },
+}) //v-myon
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: "#app",
+  render: (h) => h(App),
 })
