ac45e3c31d3b02bb6d90e9e5273f352dc2413757
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-05 22:29:48 -0300

    class 520

--- section36-animations-transtions/example/src/App.vue
+++ section36-animations-transtions/example/src/App.vue
@@ -1,24 +1,38 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Animations</h1>
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr />
+        <button class="btn btn-primary" @click="show = !show">
+          Show Alert
+        </button>
+        <hr />
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-
-            }
-        }
+export default {
+  data() {
+    return {
+      show: false,
     }
+  },
+}
 </script>
 
 <style>
-
+.fade-enter {
+}
+.fade-enter-active {
+}
+.fade-leave {
+}
+.fade-leave-active {
+}
 </style>
