a42878c3eaa22f1cefa8122467fef30bee86a01e
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-09 17:31:29 -0300

    until 602

--- section39-state-management/start/src/components/Counter.vue
+++ section39-state-management/start/src/components/Counter.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
-    <button class="btn btn-primary" @click="increment">Increment</button>
-    <button class="btn btn-primary" @click="decrement">Decrement</button>
+    <button class="btn btn-primary" @click="increment(100)">Increment</button>
+    <button class="btn btn-primary" @click="decrement(50)">Decrement</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   methods: {
-    increment() {
-      this.$store.commit('increment')
-    },
-    decrement() {
-      this.$store.commit('decrement')
-    },
+    ...mapActions(['increment', 'decrement']),
   },
 }
 </script>
