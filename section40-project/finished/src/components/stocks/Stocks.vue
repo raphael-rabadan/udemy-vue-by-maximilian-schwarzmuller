b7efa389b9f9d3426e72145050eda1c38a3b071b
Raphael Rabadan <raphael.rabadan@gmail.com>
2021-04-11 13:22:02 -0300

    project finished

---
+++ section40-project/finished/src/components/stocks/Stocks.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <app-stock v-for="(stock, i) in stocks" :stock="stock" :key="i"></app-stock>
+  </div>
+</template>
+
+<script>
+import Stock from './Stock.vue'
+
+export default {
+  components: {
+    appStock: Stock,
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks
+    },
+  },
+}
+</script>
